43be253405faeabbb9ba350736100bdb074bb23d
Flavian <Flavianmohanraj@gmail.com>
2020-01-07 23:12:20 +0000

    test case 2

--- eqArrays.js
+++ eqArrays.js
@@ -9,5 +9,6 @@ const eqArrays = function(arr1, arr2){
   }
   return true
 }
-eqArrays([1, 2, 3], [1, 2, 3])
-eqArrays([1, 2, 3], [3, 2, 1])
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]))
+console.log(eqArrays([1, 2, 3], [3, 2, 1]))
