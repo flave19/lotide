89a95a8c6866c60734379c49e7d4548e0efae38b
Flavian <Flavianmohanraj@gmail.com>
2020-01-08 00:02:08 +0000

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(arr1, arr2){
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  for(let i=0; i< arr1.length; i++){
+      if (arr1[i] !== arr2[i]){
+        return false
+      }
+  }
+  return true
+}
+
+const assertEqual = function(actual, expected) {
+  if(actual !== expected){
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
+  }
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
