49eb730bbb80a563a43fb48e35e616975025ee4f
Flavian <Flavianmohanraj@gmail.com>
2020-01-13 18:33:48 +0000

    eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+const  eqArrays = require('../eqArrays');
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]))
+console.log(eqArrays([1, 2, 3], [3, 2, 1]))
