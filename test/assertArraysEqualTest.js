6cd8fb56c2ebd5f5a126e0927ec60280adaa8043
Flavian <Flavianmohanraj@gmail.com>
2020-01-13 19:48:59 +0000

    middle

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,14 @@
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle')
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, 5]);
+
+
+assertArraysEqual(middle([1]),[]) // => []
+assertArraysEqual(middle([1, 2]),[]) // => []
+assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
